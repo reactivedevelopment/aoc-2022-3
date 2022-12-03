27cdc6cb14e151274601af8e091d22d7987c041d
Daniel Demidko <demidko@farpost.com>
2022-12-03 10:49:34 +0500

    part two complated

--- src/main/kotlin/com/adventofcode/Day.kt
+++ src/main/kotlin/com/adventofcode/Day.kt
@@ -1,11 +1,36 @@
 package com.adventofcode
 
+import com.adventofcode.Counter.prioritySum
+import com.adventofcode.Counter.process
+
+
+private const val ORDER = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+fun priorityOf(x: Char) = ORDER.indexOf(x) + 1
+
+object Counter {
+
+  private val buf = mutableSetOf<Set<Int>>()
+
+  var prioritySum = 0L; private set
+
+  fun process(elf: Set<Int>) {
+    buf.add(elf)
+    if (buf.size < 3) {
+      return
+    }
+    prioritySum += buf.reduce { acc: Set<Int>, ints: Set<Int> -> acc.intersect(ints) }.sum()
+    buf.clear()
+  }
+}
+
 fun process(line: String) {
-  TODO()
+  val elf = line.map(::priorityOf).toSet()
+  process(elf)
 }
 
 fun solution(): Long {
-  TODO()
+  return prioritySum
 }
 
 fun main() {
